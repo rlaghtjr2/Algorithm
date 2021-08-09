54e9e9fb6f5266a4d8bb7c35b26267708378ef34
김호석 <hoseok76081024@gmail.com>
2021-08-09 18:20:23 +0900

    2021-08-09
    2021-08-09

---
+++ 알고리즘_스터디/1.백준/0.free/Algo15654.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Algo15654 {
+
+	static int N,M;
+	static int[] nums,ans;
+	static boolean[] v;
+	static BufferedWriter bw;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input = br.readLine().split(" ");
+		
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+		nums = new int[N];
+		ans = new int[M];
+		v = new boolean[N];
+		
+		input = br.readLine().split(" ");
+		for(int i=0;i<input.length;i++) {
+			nums[i] = Integer.parseInt(input[i]);
+		}
+		Arrays.sort(nums);
+		comb(0);
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void comb(int cnt) throws IOException {
+		if(cnt == M) {
+			String s="";
+			for(int i=0;i<M;i++) {
+				s+=(ans[i]+" ");
+			}
+			bw.write(s+"\n");
+			return;
+		}
+		
+		for(int i=0;i<N;i++) {
+			if(v[i]) continue;
+			
+			v[i] = true;
+			ans[cnt] = nums[i];
+			comb(cnt+1);
+			v[i] = false;
+		}
+	}
+
+}
